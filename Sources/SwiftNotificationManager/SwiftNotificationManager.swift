ca8f993d758247a2f847c3fbc9913bae2024d334
Henrique Alves <henriqueasemmer@gmail.com>
2024-03-12 15:29:08 -0300

    Funções suporte de agendar notificações adicionadas

--- Sources/SwiftNotificationManager/SwiftNotificationManager.swift
+++ Sources/SwiftNotificationManager/SwiftNotificationManager.swift
@@ -18,4 +18,27 @@ public class NotificationManager: ObservableObject {
             }
         }
     }
+    
+    func setupContent(title: String, subtitle: String?, body: String, sound: UNNotificationSound?) -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle ?? ""
+        content.body = body
+        content.sound = sound ?? UNNotificationSound.default
+        
+        return content
+    }
+    
+    func scheduleNotification(identifier: String?, content: UNMutableNotificationContent, trigger: UNNotificationTrigger) {
+        let request = UNNotificationRequest(identifier: identifier ?? UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Notification scheduled")
+            }
+        }
+    }
+    
 }
