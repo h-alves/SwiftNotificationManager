22581987d056529da11c71253ecf0eb06a88b145
Henrique Alves <henriqueasemmer@gmail.com>
2024-03-12 14:44:11 -0300

    Initial Commit

---
+++ Sources/SwiftNotificationManager/SwiftNotificationManager.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+import UserNotifications
+
+@available(iOS 13.0, *)
+class NotificationManager: ObservableObject {
+    
+    static let shared = NotificationManager()
+    
+}
